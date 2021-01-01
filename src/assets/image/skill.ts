e6725581798553426ed976b947e1d283fe002f3a
Vincent Kollin <vincent.kollin@check24.de>
2021-01-01 16:33:40 +0100

    more skills

--- src/assets/image/skill.ts
+++ src/assets/image/skill.ts
@@ -4,5 +4,9 @@ export const logos: { [_: string]: string } = {
     python: require('../image/skill/python.png').default,
     react: require('../image/skill/react.png').default,
     symfony: require('../image/skill/symfony.png').default,
-    elasticsearch: require('../image/skill/elasticsearch.png').default
+    elasticsearch: require('../image/skill/elasticsearch.png').default,
+    git: require('../image/skill/git.png').default,
+    javascript: require('../image/skill/javascript.png').default,
+    nginx: require('../image/skill/nginx.png').default,
+    sass: require('../image/skill/sass.png').default,
 }
