c5ba3b01f6698c93d15a9248f535b6576c7c2607
Vincent Kollin <vincent.kollin@check24.de>
2021-01-01 16:14:59 +0100

    enable source map, pass mode as parameter, remove imagePlugin

--- src/webpack.config.babel.js
+++ src/webpack.config.babel.js
@@ -5,101 +5,97 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const autoprefixer = require('autoprefixer')
-const ImageminPlugin = require('imagemin-webpack-plugin').default
-const imageminMozjpeg = require('imagemin-mozjpeg')
 
-const isDevelopment = true
 
-let outputDir = path.resolve(__dirname, '../dist');
-let assetsDir = path.join(__dirname, '/assets');
+module.exports = (env, argv) => {
+    const outputDir = path.resolve(__dirname, '../dist');
+    const assetsDir = path.join(__dirname, '/assets');
 
-const config = {
-    mode: isDevelopment ? 'development' : 'production',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx', '.css', '.scss'],
-    },
-    entry: {
-        index: path.join(assetsDir, "index.jsx")
-    },
+    const isDevelopment = argv.mode === 'development';
 
-    output: {
-        path: outputDir,
-        filename: '[name].[fullhash].js'
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: isDevelopment ? '[name].css' : '[name].[fullhash].css',
-            chunkFilename: isDevelopment ? '[id].css' : '[id].[fullhash].css',
-        }),
-        new CleanWebpackPlugin({cleanStaleWebpackAssets: !isDevelopment}),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.html')
-        }),
-        new ImageminPlugin({
+    const config = {
+        mode: isDevelopment ? 'development' : 'production',
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.jsx', '.css', '.scss'],
+        },
+        entry: {
+            index: path.join(assetsDir, "index.jsx")
+        },
 
-            pngquant: {quality: [0.5, 0.5]},
-            plugins: [imageminMozjpeg({quality: 50})]
-        })
-    ],
+        output: {
+            path: outputDir,
+            filename: '[name].[fullhash].js'
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: isDevelopment ? '[name].css' : '[name].[fullhash].css',
+                chunkFilename: isDevelopment ? '[id].css' : '[id].[fullhash].css',
+            }),
+            new CleanWebpackPlugin({cleanStaleWebpackAssets: !isDevelopment}),
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'index.html')
+            }),
+        ],
 
-    optimization: {
-        minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin({})],
-        splitChunks: {
-            chunks: 'all',
-            maxAsyncRequests: 10,
-            maxInitialRequests: 6,
-            cacheGroups: {
-                defaultVendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10,
-                },
-                default: {
-                    priority: -20,
-                    reuseExistingChunk: true,
+        optimization: {
+            minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin({})],
+            splitChunks: {
+                chunks: 'all',
+                maxAsyncRequests: 10,
+                maxInitialRequests: 6,
+                cacheGroups: {
+                    defaultVendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10,
+                    },
+                    default: {
+                        priority: -20,
+                        reuseExistingChunk: true,
+                    },
                 },
             },
         },
-    },
 
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                ident: 'postcss',
-                                plugins: [autoprefixer]
+        module: {
+            rules: [
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    ident: 'postcss',
+                                    plugins: [autoprefixer]
+                                }
                             }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.(j|t)sx?$/,
-                exclude: /(node_modules|prod)/,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/i,
-                use: ['file-loader']
-            },
-            {
-                test: /\.html$/,
-                use: [{loader: 'html-loader'}]
-            },
-        ]
+                    ]
+                },
+                {
+                    test: /\.(j|t)sx?$/,
+                    exclude: /(node_modules|prod)/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/i,
+                    use: ['file-loader']
+                },
+                {
+                    test: /\.html$/,
+                    use: [{loader: 'html-loader'}]
+                },
+            ]
+        }
     }
-}
 
-if (isDevelopment) {
-    configdevtool = 'eval-cheap-source-map';
-    config.devServer = {contentBase: path.join(__dirname, 'dist'), port: 3000, hot: true}
-}
+    if (isDevelopment) {
+        config.devtool = 'eval-cheap-source-map';
+        config.devServer = {contentBase: path.join(__dirname, 'dist'), port: 3000, hot: true}
+    }
 
-module.exports = config
+    return config;
+}
